ee1ab4703f5301d348f9a2f61329c679f5077c60
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:09:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 def load_library (file_path)
   require "yaml"
   emojis = YAML.load_file(file_path)
-  translator_hash = {:get_meaning => {}, :get_emoticon => {}}
+  translator_hash = {get_meaning : {}, get_emoticon : {}}
   #emojis.each do |meaning, emoticons|
     #:get_meaning[meaning[0]] = meaning
   #  translator_hash[:get_meaning][emoticons[1]] = meaning
