94308866d218c00a100a2fbaeb48e30d2d4cd383
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:35:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,10 @@ end
 
 def get_japanese_emoticon (file_path, emoticon) #takes Western emoticon, :) and translate it to Japanese emoticon
   load_library(file_path)
+  if translator_hash[:get_emoticon][emoticon]
   translator_hash[:get_emoticon][emoticon]
+else "Sorry"
+end
 end
 
 def get_english_meaning
