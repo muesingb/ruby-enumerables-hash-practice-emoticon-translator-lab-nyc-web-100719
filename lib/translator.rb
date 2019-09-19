5af30d007c6ae1cb0a965ce3580cf42ed7142f89
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:36:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,6 @@ def get_japanese_emoticon (file_path, emoticon) #takes Western emoticon, :) and 
 #end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (file_path, emoticon)
+  load_library(file_path)
 end
