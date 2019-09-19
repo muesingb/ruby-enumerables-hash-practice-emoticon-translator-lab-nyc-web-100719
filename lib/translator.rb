9079673d7f8775f17881c77a5b4e5c0066cf0a27
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:28:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library (file_path)
   translator_hash = {:get_meaning => {}, :get_emoticon => {}}
   emojis.each do |meaning_key, emoticons_value|
     (translator_hash[:get_meaning])[emoticons_value[1]] = meaning_key
-    (translator_hash[:get_emoticon])[emoticons_value[0]] = emoticons_value[0]
+    (translator_hash[:get_emoticon])[emoticons_value[0]] = emoticons_value[1]
   end
 return translator_hash
 end
