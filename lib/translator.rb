3d60d3c26fc4f52cb33343b1306220da9c10a2b3
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:48:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def get_japanese_emoticon (file_path, emoticon) #takes Western emoticon, :) and 
   if load_library(file_path)[:get_emoticon][emoticon]
   load_library(file_path)[:get_emoticon][emoticon]
   else "Sorry, that emoticon was not found"
-end
+  end
 end
 
 def get_english_meaning (file_path, emoticon) #takes a Japanese emoticon and returns its meaning in Englis
