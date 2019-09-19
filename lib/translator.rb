b095b7cabdc5dc3b24ac897a88423ca8327785fd
Barbara Muesing <muesingb@gmail.com>
2019-09-19 20:40:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,8 @@ return translator_hash
 end
 
 def get_japanese_emoticon (file_path, emoticon) #takes Western emoticon, :) and translate it to Japanese emoticon
-  load_library(file_path)
-  #if translator_hash[:get_emoticon][emoticon]
+  library = load_library(file_path)
+  if library[translator_hash][:get_emoticon][emoticon]
   #translator_hash[:get_emoticon][emoticon]
 #else "Sorry, that emoticon was not found"
 #end
